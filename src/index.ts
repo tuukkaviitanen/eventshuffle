c302b2ac4fe35d73c44a0d8e253be0616463f709
Tuukka Viitanen <97726090+tuukkaviitanen@users.noreply.github.com>
2025-06-23 13:02:08 +0300

    Added API routes with dummy responses (#10)
    * Added API routes with dummy responses
    
    * Fixed formatting errors

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 import { Elysia } from "elysia";
+import eventRoutes from "./routes/eventRoutes";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia({ prefix: "/api/v1" }).use(eventRoutes).listen(3000);
 
-console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`,
-);
+console.log(`🦊 Elysia is running at port ${app.server?.port}`);
